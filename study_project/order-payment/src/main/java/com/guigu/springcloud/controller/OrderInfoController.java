9b91fffee18d5fc0be9a78162b029ebc9a368944
providencejune <478054313@qq.com>
2021-05-25 20:10:38 +0800

    eureka 配置相关

--- study_project/order-payment/src/main/java/com/guigu/springcloud/controller/OrderInfoController.java
+++ study_project/order-payment/src/main/java/com/guigu/springcloud/controller/OrderInfoController.java
@@ -6,6 +6,8 @@ import com.guigu.springcloud.service.OrderInfoService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -21,6 +24,8 @@ public class OrderInfoController {
     private OrderInfoService orderInfoService;
     @Value("${server.port}")
     private String servePort;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/order/create")
     public CommonResult<Integer> createOrderInfo(OrderInfoEntity entity) {
@@ -38,5 +43,19 @@ public class OrderInfoController {
         OrderInfoEntity rs = orderInfoService.getById(id);
         log.info("服务端口:" + servePort + ";获取对应订单：" + rs);
         return new CommonResult(200, "服务端口:" + servePort, rs);
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String serve : services) {
+            log.info("****element:" + serve);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-ORDER-PAYMENT");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
