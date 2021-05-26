6f2057a0c6bff5883959fb70a4c29c98ae8b698f
providencejune <478054313@qq.com>
2021-05-26 22:08:48 +0800

    zookeeper 安装zookeeper 安装 服务注册到zookeeper 的流程 服务注册到zookeeper 的流程

---
+++ study_project/order-zk-payment3/src/main/java/com/guigu/springcloud/controller/paymentController.java
@@ -0,0 +1,18 @@
+package com.guigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class paymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String payment() {
+        return "springcloud with zookeeper:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
